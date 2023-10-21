a86caecad7b11dcb61f46e68d1f04a033c41cfd0
MuhammadAinurRofik <124505800+22091397005MuhammadAinurRofik@users.noreply.github.com>
2023-10-21 14:56:57 +0700

    Delete js.js

--- js.js
+++
@@ -1,9 +0,0 @@
-function openPopup(buku) {
-  const notif = document.getElementById(`notif-${buku}`);
-  notif.style.display = "block";
-}
-
-function closePopup(buku) {
-  const notif = document.getElementById(`notif-${buku}`);
-  notif.style.display = "none";
-}
